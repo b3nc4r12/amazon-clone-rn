4fdf44be1ae0196d0f9b61cfda502e82b46c7cc0
Ben Carter <benjcarter06@gmail.com>
2021-11-11 19:43:55 -0400

    update tabs to show cart length

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -1,7 +1,9 @@
 import React from "react"
-import { View, Pressable } from "react-native"
+import { View, Pressable, Text } from "react-native"
 import { Icon } from "react-native-elements"
 import tw from "tailwind-react-native-classnames"
+import { useRecoilState, useRecoilValue } from "recoil"
+import { cartState } from "../atoms/cartAtom"
 
 const icons = [
     {
@@ -29,9 +31,10 @@ const icons = [
 const BottomTabs = ({ activeScreen, setActiveScreen }) => {
     return (
         <View style={tw`absolute bottom-0 left-0 right-0 bg-white h-20 border-t border-gray-300 shadow-md flex-row justify-around`}>
-            {icons.map((icon) => (
+            {icons.map((icon, index) => (
                 <BottomIcon
-                    key={icon.name}
+                    key={index}
+                    index={index}
                     name={icon.iconName}
                     type={icon.iconType}
                     color={activeScreen === icon.name ? "#4397a4" : "#000"}
@@ -43,13 +46,24 @@ const BottomTabs = ({ activeScreen, setActiveScreen }) => {
     )
 }
 
-const BottomIcon = ({ name, type, color, borderStatus, onPress }) => (
-    <Pressable onPress={onPress} style={tw`relative pt-2.5 w-10`}>
-        {borderStatus && (
-            <View style={[tw`absolute top-0 left-0 right-0 border-t-4 rounded-b-md`, { borderColor: color }]} />
-        )}
-        <Icon type={type} name={name} size={30} color={color} />
-    </Pressable>
-)
+const BottomIcon = ({ name, type, color, borderStatus, onPress, index }) => {
+    const [cart] = useRecoilState(cartState);
+
+    return (
+        <Pressable onPress={onPress} style={tw`relative pt-2.5 w-10`}>
+            {borderStatus && (
+                <View style={[tw`absolute top-0 left-0 right-0 border-t-4 rounded-b-md`, { borderColor: color }]} />
+            )}
+            <View style={tw`relative`}>
+                {index === 2 && (
+                    <View style={tw`absolute -top-1 right-0 h-4 w-4 bg-yellow-500 rounded-full justify-center items-center z-50`}>
+                        <Text style={tw`font-extrabold text-xs`}>{cart.length ? cart.length : "0"}</Text>
+                    </View>
+                )}
+                <Icon type={type} name={name} size={30} color={color} />
+            </View>
+        </Pressable>
+    )
+}
 
 export default BottomTabs
